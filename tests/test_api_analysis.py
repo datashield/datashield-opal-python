7b45a51b1ba6bee8677a94c336ebdd27b88b19e3
Yannick Marcon <yannick.marcon@obiba.org>
2026-02-14 17:24:52 +0100

    fix: tests on resources

--- tests/test_api_analysis.py
+++ tests/test_api_analysis.py
@@ -1,4 +1,4 @@
-from datashield import DSSession, DSLoginBuilder
+from datashield import DSSession, DSLoginBuilder, DSError
 
 
 class TestClass:
@@ -14,49 +14,65 @@ class TestClass:
         self.session.close()
 
     def test_assign_tables(self):
-        self.session.assign_table(
-            "df", tables={"server1": "CNSIM.CNSIM1", "server2": "CNSIM.CNSIM2"}, asynchronous=False
-        )
-        rval = self.session.ls()
-        assert len(rval) == 2
-        assert "server1" in rval
-        assert rval["server1"] == ["df"]
-        assert "server2" in rval
-        assert rval["server2"] == ["df"]
+        try:
+            self.session.assign_table(
+                "df", tables={"server1": "CNSIM.CNSIM1", "server2": "CNSIM.CNSIM2"}, asynchronous=False
+            )
+            rval = self.session.ls()
+            assert len(rval) == 2
+            assert "server1" in rval
+            assert rval["server1"] == ["df"]
+            assert "server2" in rval
+            assert rval["server2"] == ["df"]
+        except DSError as e:
+            print(self.session.get_errors())
+            raise ValueError("Failed to assign tables") from e
 
     def test_assign_table(self):
-        self.session.assign_table("df", table="CNSIM.CNSIM1", asynchronous=False)
-        rval = self.session.ls()
-        assert len(rval) == 2
-        assert "server1" in rval
-        assert rval["server1"] == ["df"]
-        assert "server2" in rval
-        assert rval["server2"] == ["df"]
+        try:
+            self.session.assign_table("df", table="CNSIM.CNSIM1", asynchronous=False)
+            rval = self.session.ls()
+            assert len(rval) == 2
+            assert "server1" in rval
+            assert rval["server1"] == ["df"]
+            assert "server2" in rval
+            assert rval["server2"] == ["df"]
+        except DSError as e:
+            print(self.session.get_errors())
+            raise ValueError("Failed to assign table") from e
 
     def test_assign_resources(self):
-        self.session.assign_resource(
-            "client", resources={"server1": "RSRC.CNSIM1", "server2": "RSRC.CNSIM2"}, asynchronous=False
-        )
-        rval = self.session.ls()
-        assert len(rval) == 2
-        assert "server1" in rval
-        assert rval["server1"] == ["client"]
-        assert "server2" in rval
-        assert rval["server2"] == ["client"]
+        try:
+            self.session.assign_resource(
+                "client", resources={"server1": "RSRC.CNSIM1", "server2": "RSRC.CNSIM1"}, asynchronous=False
+            )
+            rval = self.session.ls()
+            assert len(rval) == 2
+            assert "server1" in rval
+            assert rval["server1"] == ["client"]
+            assert "server2" in rval
+            assert rval["server2"] == ["client"]
 
-        self.session.assign_expr("df", "as.resource.data.frame(client, strict = TRUE)", asynchronous=False)
-        rval = self.session.ls()
-        assert len(rval) == 2
-        assert "server1" in rval
-        assert len(rval["server1"]) == 2
-        assert "client" in rval["server1"]
-        assert "df" in rval["server1"]
+            self.session.assign_expr("df", "as.resource.data.frame(client, strict = TRUE)", asynchronous=False)
+            rval = self.session.ls()
+            assert len(rval) == 2
+            assert "server1" in rval
+            assert len(rval["server1"]) == 2
+            assert "client" in rval["server1"]
+            assert "df" in rval["server1"]
+        except DSError as e:
+            print(self.session.get_errors())
+            raise ValueError("Failed to assign resources") from e
 
     def test_assign_resource(self):
-        self.session.assign_resource("client", resource="RSRC.CNSIM1", asynchronous=False)
-        rval = self.session.ls()
-        assert len(rval) == 2
-        assert "server1" in rval
-        assert rval["server1"] == ["client"]
-        assert "server2" in rval
-        assert rval["server2"] == ["client"]
+        try:
+            self.session.assign_resource("client", resource="RSRC.CNSIM1", asynchronous=False)
+            rval = self.session.ls()
+            assert len(rval) == 2
+            assert "server1" in rval
+            assert rval["server1"] == ["client"]
+            assert "server2" in rval
+            assert rval["server2"] == ["client"]
+        except DSError as e:
+            print(self.session.get_errors())
+            raise ValueError("Failed to assign resource") from e
